373fe18e092534a5a6fbe97f0c809c9377427ce7
brunaguarizo <b.guarizo@hotmail.com>
2025-10-22 12:31:24 -0700

    Anna Avatar

---
+++ .history/components/AnnaAvatar/AnnaAvatar_20251022122940.jsx
@@ -0,0 +1,13 @@
+import styles from "./AnnaAvatar.module.css";
+
+function AnnaAvatar() {
+    return (
+        <img
+            className={styles.image}
+            src='AnnaAvatar.svg'
+            alt='Anna Avatar'
+        />
+    );
+}
+
+export default AnnaAvatar;
