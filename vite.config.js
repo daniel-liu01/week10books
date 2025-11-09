e9cb2bb99265e443eab8176a5ff9674451bd839a
Daniel Liu <mliu192@se14l0120-07.ad.bcit.ca>
2025-11-08 19:25:19 -0800

    fix directory

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/week9books/",
+  base: "/week10books/", // ✅ must match your GitHub repo name
 });
